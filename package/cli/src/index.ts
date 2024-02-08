bbe01e771996e40ba24c358c66fb3361d8716031
Diwanshu <saidiwanshu1880@gmail.com>
2024-02-08 18:59:34 +0530

    Added Node Types fixed Commander

--- package/cli/src/index.ts
+++ package/cli/src/index.ts
@@ -1,2 +1,12 @@
 #!/usr/bin/env node
-console.log('Hello World')
+import { program } from 'commander'
+
+function AddCommand() {}
+
+program
+    .name('add')
+    .description('For adding functions and utilities for node')
+    .command('add')
+    .action(AddCommand)
+
+program.parse(process.argv)
